8f8da832e9de77b3ac4b315c7915fa1e1d535dbe
streethacker <silverbullet7714@126.com>
2014-10-19 23:26:32 +0800

    add docs for dequeImpl.py

--- dequeImpl.py
+++ dequeImpl.py
@@ -4,7 +4,17 @@
 from doublyLinkedListImpl import DoublyLinkedNode, DoublyLinkedList
 
 class Deque(DoublyLinkedList):
+	"""
+	a simple double ended queue implementation based on doubly linked list.
+	inheritted from the DoublyLinkedList, so that we can reuse some of the
+	operations defined in the doubly linked list data structure.
+	"""
 	def dequeueFront(self):
+		"""
+		dequeue from the left end of the queue, that is, remove the first node of the doubly linked list.
+		it's much simpler than removeItem() operation of the standard doubly linked list, because all the
+		deletion occurs at the beginning end, and we can make extensive use of the head pointer.
+		"""
 		if self._linkHead == None:
 			return False
 
@@ -23,6 +33,10 @@ class Deque(DoublyLinkedList):
 		return _nodeReturn._itemValue
 
 	def dequeueBack(self):
+		"""
+		dequeue from the right end of the queue, opposite to the dequeueFront() method. make full use of the
+		tail pointer.
+		"""
 		if self._linkHead == None:
 			return False
 
@@ -41,6 +55,10 @@ class Deque(DoublyLinkedList):
 		return _nodeReturn._itemValue
 
 	def enqueueFront(self, value):
+		"""
+		enqueue from the left end, much simpler than a standard insertion of the doubly linked list, due to
+		the head pointer.
+		"""
 		_linkedNode = DoublyLinkedNode(value)
 
 		if self._linkHead == None:
@@ -56,24 +74,41 @@ class Deque(DoublyLinkedList):
 		return True
 
 	def enqueueBack(self, value):
+		"""
+		enqueue at the right end, opposite to the enqueueFront() method, the same as the standard addItem() operation,
+		and benefit from the inheritance, just call the addItem() method directly.
+		"""
 		super(Deque, self).addItem(value)
 
 	def peekFront(self):
+		"""
+		unlike the dequeueFront, peekFront() method just return the left-most node(pointed by head pointer)
+		without removing it.
+		"""
 		if self._linkHead == None:
 			return False
 
 		return self._linkHead._itemValue
 
 	def peekBack(self):
+		"""
+		opposite to the peekFront() method.
+		"""
 		if self._linkHead == None:
 			return False
 
 		return self._linkTail._itemValue
 
 	def traverseDeque(self, func):
+		"""
+		the same as the standard traverseList() operation.
+		"""
 		super(Deque, self).traverseList(func)
 
 	def reverseTraverseDeque(self, func):
+		"""
+		the same as the standard reverseTraverseList() operation.
+		"""
 		super(Deque, self).reverseTraverseList(func)
 
 if __name__ == "__main__":
